7101284dc54b8b45c0b95509bba171dbab8278bd
artzxc13 <artyom.kharitonov27@gmail.com>
2022-11-18 13:01:32 +0100

    new update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import Categories from './Categories';
 import ShoeBlock from './ShoeBlock';
 import SortPopup from './SortPopup';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCategory } from '../redux/actions/filters';
 
-function Home({ items }) {
+const categoryesName = ['Sneakers', 'Heritage', 'Will Be'];
+const sortItems = [
+  { name: 'popular', type: 'popular' },
+  { name: 'priсe', type: 'priсe' },
+  { name: 'alphabetically', type: 'alphabetically' },
+];
+function Home() {
+  const dispatch = useDispatch();
+
+  const items = useSelector(({ shoesReducer }) => shoesReducer.items);
+
+  const onSelectCatagory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          onClick={(name) => console.log(name)}
-          items={['Sneakers', 'Heritage', 'Will Be']}
-        />
-        <SortPopup
-          onClick={(name) => console.log(name)}
-          items={[
-            { name: 'popular', type: 'popular' },
-            { name: 'priсe', type: 'priсe' },
-            { name: 'alphabetically', type: 'alphabetically' },
-          ]}
-        />
+        <Categories onClickItem={onSelectCatagory} items={categoryesName} />
+        <SortPopup items={sortItems} />
       </div>
       <h2 className="content__title">All shoes</h2>
       <div className="content__items">
